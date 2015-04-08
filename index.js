babe8485d6c7794f30526a4eb0d369bec5c77ada
wfsovereign <never_fall@sohu.com>
2015-04-08 16:46:30 +0800

    has been all data from session storage,ready to write spec

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
-/**
- * Created by wfsovereign on 15-4-7.
- */
+function getFillingInBlank() {
+    var fillingInBlank = {};
+    fillingInBlank.one = $('#fillingInBlank1').val();
+    fillingInBlank.two = [];
+    fillingInBlank.two.push($('#fillingInBlank2-1').val());
+    fillingInBlank.two.push($('#fillingInBlank2-2').val());
+    fillingInBlank.two.push($('#fillingInBlank2-3').val());
+    return fillingInBlank;
+}
+
+function getSingleOption() {
+    var singleOption = [];
+    singleOption.push($("input[name='two1']:checked").val());
+    singleOption.push($("input[name='two2']:checked").val());
+    return singleOption;
+}
+
+function getMultipleOption() {
+    var multipleOption = {
+        one:[],
+        two:[]
+    };
+    $("input[name='three1']:checked").each(function () {
+        multipleOption.one.push(this.value);
+     });
+    $("input[name='three2']:checked").each(function () {
+        multipleOption.two.push(this.value);
+     });
+
+    return multipleOption;
+}
+
+
+function getJudgeResult() {
+    var judgeResult = [];
+    judgeResult.push($("input[name='judge1']:checked").val());
+    judgeResult.push($("input[name='judge2']:checked").val());
+    return judgeResult;
+}
+
+function getStudentInfo() {
+    var studentInfo = {};
+    studentInfo.name = $('#studentName').val();
+    studentInfo.id = $('#studentId').val();
+    studentInfo.class = $('#studentClass').val();
+    return studentInfo
+}
+
+function setSessionStorageItem(name, data) {
+    for(var i=0;i<name.length;i++){
+        sessionStorage.setItem(name[i],Json.stringify(data[i]));
+    }
+}
+
 
 $(function () {
-   $("#submit").on('click',function (){
-       console.log("----");
+    $("#submit").on('click', function () {
+        var studentInfo = getStudentInfo();
+        var fillingInBlank = getFillingInBlank();
+        var singleOption = getSingleOption();
+        var multipleOption = getMultipleOption();
+        var judgeResult = getJudgeResult();
 
-     $("input[name='three1']:checked").each(function (){
-               console.log(this.value);
-       });
-       console.log($("input[name='three2']:checked").val());
-       //console.log($("#fillingInBlank1").val());
-       console.log("----");
-       console.log($("#studentClass").width());
-   })
+        var pageInfo = [studentInfo,fillingInBlank,singleOption,multipleOption,judgeResult];
+        var storageNameArray = ["studentInfo","fillingInBlank","singleOption","multipleOption","judgeResult"];
+
+        setSessionStorageItem(storageNameArray,pageInfo);
+
+    })
 });
+
+
